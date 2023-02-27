98a0c414f3782e56bf2453695df60a319c24e626
HarindiPerera <harindiperera@dandelions.cloud>
2023-02-27 16:07:46 +1100

    added a queue

--- sample_project/main/main.c
+++ sample_project/main/main.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/timer.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_system.h"
+#include "esp_err.h"
+#include "DandSMC.h"
+
+QueueHandle_t experimentQueue = NULL;
 
 void app_main(void)
 {
